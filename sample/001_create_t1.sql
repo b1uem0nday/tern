94fbd76b4be9ceccb52bae6d96060faa8882fd63
Default Name <vitunsaatanaperkele@gmail.com>
2022-07-20 16:15:57 +0300

    оставлен основной функционал

--- sample/001_create_t1.sql
+++
@@ -1,8 +0,0 @@
-create table t1(
-  id serial primary key,
-  placebo varchar not null
-);
-
----- create above / drop below ----
-
-drop table t1;
--- sample/shared/v1_001.sql
+++
@@ -1,1 +0,0 @@
-create view v1 as select * from t1;
